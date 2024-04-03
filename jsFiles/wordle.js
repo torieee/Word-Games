1949357767aa6b52c5ae5484ab868d7f465baf9e
Torie <victoriamthacker@gmail.com>
2024-04-03 10:58:46 -0400

    wordle ui setup

--- jsFiles/wordle.js
+++ jsFiles/wordle.js
@@ -2,17 +2,23 @@ var wordle = (() => {
 
     let correctLettersWrongPlace = [];
     let correctLettersRightPlace = [];
+    let wordle;
 
-    async function generateWordle(event){
+    async function startGame(event){
         event.preventDefault();
+        wordle = await generateWordle();
+        console.log(wordle);
+    }
+
+    async function generateWordle(){
         const wordle = await process.generateWord(5);
-        console.log("Wordle works! ", wordle);
+        return wordle;
     }
 
 
 
 
     return {
-        generateWordle,
+        startGame,
     };
   })();
