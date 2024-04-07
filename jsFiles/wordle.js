8cb9be0a3affc51120845de2a0ae0624137c3d14
Torie Thacker <victoriamthacker@gmail.com>
2024-04-07 12:33:13 -0400

    refactored loadGrid method

--- jsFiles/wordle.js
+++ jsFiles/wordle.js
@@ -35,38 +35,11 @@ var wordle = (() => {
 
     function loadGrid() {
         const guessInputsContainer = document.getElementById('guess-inputs');
-        const wordLength = 5;
-        const numRows = 6;
     
-        for (let rows = 0; rows < numRows; rows++) {
-            for (let i = 0; i < wordLength; i++) { //CURRENT ROW / WORD
+        for (let rows = 0; rows < 6; rows++) { //All rows
+            for (let i = 0; i < 5; i++) { //CURRENT ROW / WORD
                 const input = document.createElement('input');
-                input.type = 'text';
-                input.setAttribute('maxlength', '1');
-                const inputName = rows + "" + i;
-                input.id = inputName;
-                input.addEventListener('input', function() {
-                    const nextInputIndex = (rows * wordLength) + (i + 1);
-                    const nextInput = document.querySelectorAll('#guess-inputs input')[nextInputIndex];
-                    if (nextInput && this.value.length === 1) {
-                        nextInput.focus();
-                    }
-                });
-                input.addEventListener('keydown', function(event) {
-                    if (event.key === 'Backspace' && this.value.length === 0) {
-                        const previousInputIndex = (rows * wordLength) + (i - 1);
-                        const previousInput = document.querySelectorAll('#guess-inputs input')[previousInputIndex];
-                        if (previousInput) {
-                            previousInput.focus();
-                        }
-                    }
-                    
-                    // Check if the Enter key is pressed and the input is the last column
-                    if (event.key === 'Enter' && i === (wordLength - 1)) {
-                        event.preventDefault(); // Prevent default form submission behavior
-                        submitGuess(event); // Call the submitGuess function
-                    }
-                });
+                setUpInputElements(i, rows, input);
                 guessInputsContainer.appendChild(input);
             }
         }
@@ -86,6 +59,45 @@ var wordle = (() => {
                 const inputBox = document.getElementById(boxName);
                 inputBox.setAttribute('disabled', 'true');
             }
+        }
+    }
+
+    function setUpInputElements(i, rows, input){
+        input.type = 'text';
+        input.setAttribute('maxlength', '1');
+        const inputName = rows + "" + i;
+        input.id = inputName;
+
+        input.addEventListener('input', function() {
+            focusNextInput(i, rows, this);
+        });
+
+        input.addEventListener('keydown', function(event) {
+            handleKeyFunctions(event, this, rows, i);
+        });
+    }
+
+    function focusNextInput(i, rows, inputBox){
+        inputBox.value = inputBox.value.toUpperCase();
+        const nextInputIndex = (rows * 5) + (i + 1);
+        const nextInput = document.querySelectorAll('#guess-inputs input')[nextInputIndex];
+        if (nextInput && inputBox.value.length === 1) {
+            nextInput.focus();
+        }
+    }
+
+    function handleKeyFunctions(event, inputBox, rows, i){
+        if (event.key === 'Backspace' && inputBox.value.length === 0) {
+            const previousInputIndex = (rows * 5) + (i - 1);
+            const previousInput = document.querySelectorAll('#guess-inputs input')[previousInputIndex];
+            if (previousInput) {
+                previousInput.focus();
+            }
+        }
+        
+        if (event.key === 'Enter' && i === 4) {
+            event.preventDefault();
+            submitGuess(event);
         }
     }
 
