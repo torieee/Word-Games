491c4cc811b9f963e20713a3dd20d4a814d4c14c
Torie Thacker <victoriamthacker@gmail.com>
2024-04-10 09:50:43 -0400

    letter guess functionality partially working

--- jsFiles/wordle.js
+++ jsFiles/wordle.js
@@ -7,12 +7,14 @@ var wordle = (() => {
 
     async function startGame(event){
         event.preventDefault();
+        loadGrid();
         wordle = await generateWordle();
         console.log(wordle);
     }
 
     async function generateWordle(){
-        const wordle = await process.generateWord(5);
+        var wordle = await process.generateWord(5);
+        wordle = wordle.toUpperCase();
         return wordle;
     }
 
@@ -22,33 +24,58 @@ var wordle = (() => {
         //put in string array to string
         //check the letters against the original word
 
+        //CHECK IF WORD IS A VALID WORD BEFORE DOING ANY OF THIS
+
         let guessedWord = [];
-        for (let i = 0; i < 5; i++){
-            const elementID = guessCounter + "" + i;
-            console.log("Element ID: ", elementID);
+        for (let i = 0; i < 5; i++){ //Letters in guessed word
+            const elementID = guessCounter + "" + i; //Element id of the current letter
             const letter = document.getElementById(elementID).value;
             guessedWord[i] = letter;
         }
         console.log(guessedWord);
+        checkGuess(guessedWord);
+
+        //ENABLE AND DISABLE ROWS
+
         guessCounter++;
+        disableRows(guessCounter);
+    }
+
+    function checkGuess(wordArray){
+        for(let i = 0; i < 5; i++){
+            const elementID = guessCounter + "" + i; //Element id of the current letter
+            const letter = document.getElementById(elementID);
+            if(wordle[i] == wordArray[i]){
+                correctLettersRightPlace[i] = wordArray[i];
+                console.log("weve encountered a letter in the correct place: ", wordArray[i]);
+                letter.setAttribute('style', 'text-align: center; background-color: green; color: white');
+                continue;
+            }
+            if(wordle.includes(wordArray[i])){
+                correctLettersWrongPlace[i] = wordArray[i]; //NEED TO CHANGE THIS
+                console.log("weve encountered a good letter in the wrong place: ", wordArray[i]);
+                letter.setAttribute('style', 'text-align: center; background-color: yellow; color: black');
+                continue;
+            }
+            letter.setAttribute('style', 'text-align: center; background-color: grey; color: white');
+        }
     }
 
     function loadGrid() {
         const guessInputsContainer = document.getElementById('guess-inputs');
-    
         for (let rows = 0; rows < 6; rows++) { //All rows
-            for (let i = 0; i < 5; i++) { //CURRENT ROW / WORD
+            for (let i = 0; i < 5; i++) { //CURRENT WORD
                 const input = document.createElement('input');
                 setUpInputElements(i, rows, input);
                 guessInputsContainer.appendChild(input);
             }
         }
-    
         disableRows(0);
     }
     
 
     function disableRows(currentRow){
+        console.log("Enabling row: ", guessCounter + 1);
         for (let row = 0; row < 6; row++){
             if(currentRow === row){
                 continue;
@@ -60,6 +87,11 @@ var wordle = (() => {
                 inputBox.setAttribute('disabled', 'true');
             }
         }
+        for(let column = 0; column < 5; column ++){
+            const boxName = currentRow + "" + column;
+            const inputBox = document.getElementById(boxName);
+            inputBox.removeAttribute('disabled');
+        }
     }
 
     function setUpInputElements(i, rows, input){
@@ -67,6 +99,8 @@ var wordle = (() => {
         input.setAttribute('maxlength', '1');
         const inputName = rows + "" + i;
         input.id = inputName;
+
+        input.setAttribute('style', 'text-align: center;');
 
         input.addEventListener('input', function() {
             focusNextInput(i, rows, this);
@@ -102,8 +136,9 @@ var wordle = (() => {
     }
 
 
-    document.addEventListener('DOMContentLoaded', function() {
-        loadGrid();
+    document.addEventListener('DOMContentLoaded', function(event) {
+        //loadGrid();
+        startGame(event)
     });
 
 
